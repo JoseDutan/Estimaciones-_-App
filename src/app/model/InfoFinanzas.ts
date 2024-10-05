8e28aaed7b55e4936c9a899fd3ea907ffa3aa994
alex7est <66407825+alex7est@users.noreply.github.com>
2024-10-05 13:32:57 -0500

    model

---
+++ src/app/model/InfoFinanzas.ts
@@ -0,0 +1,9 @@
+import { ItemFinanzas } from "./ItemFinanzas";
+
+export class DetFinanciero {
+    costos_ad!:ItemFinanzas[]
+    costo_total!:ItemFinanzas
+    nomina_mensual!:ItemFinanzas
+    precio_venta!:ItemFinanzas
+    utilidad!:ItemFinanzas
+}
---
+++ src/app/model/InfoProyecto.ts
@@ -0,0 +1,7 @@
+import { Roles } from "./Roles"
+
+export class InfoProyecto {
+    requerimiento!:string
+    duracion!:number
+    roles!:Roles[]
+}
